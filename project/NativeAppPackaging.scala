17d991629fcad9dd19fc94fe4ad95a1f7be92a18
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2017-02-27 21:13:58 +0100

    Add docs to NativeApp

--- project/NativeAppPackaging.scala
+++ project/NativeAppPackaging.scala
@@ -7,9 +7,14 @@ import com.typesafe.sbt.packager.universal.UniversalPlugin
 import scala.scalanative.sbtplugin.ScalaNativePlugin
 import scala.scalanative.sbtplugin.ScalaNativePlugin.AutoImport._
 
+/**
+ * == Native App Archetype ==
+ *
+ * Bundles the native binary into a distributable package
+ */
 object NativeAppPackaging extends AutoPlugin {
 
-  override lazy val requires = UniversalPlugin && ScalaNativePlugin
+  override lazy val requires = ScalaNativePlugin && UniversalPlugin
 
   override lazy val projectSettings: Seq[Setting[_]] = Seq(
     mappings in Universal += {
